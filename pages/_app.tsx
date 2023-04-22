679dd8ec74d0a4e340ca68ca78fa645807e81251
otaki0413 <takida0413@gmail.com>
2023-04-22 15:59:32 +0900

    📈 TTFB, FCP, LCの測定処理追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,30 @@
-import type { AppProps } from "next/app";
+import type { AppProps, NextWebVitalsMetric } from "next/app";
 import "@/lib/tailwind.css";
+
+// webvitalを計測する処理
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  // 取得したメトリックに応じて分岐
+  switch (metric.name) {
+    case "FCP":
+      console.log(`FCP: ${Math.round(metric.value * 10) / 10}`);
+      break;
+    case "LCP":
+      console.log(`LCP: ${Math.round(metric.value * 10) / 10}`);
+      break;
+    case "TTFB":
+      console.log(`TTFB: ${Math.round(metric.value * 10) / 10}`);
+      break;
+    case "Next.js-hydration":
+      console.log(
+        `Hydration: ${Math.round(metric.startTime * 10) / 10} -> ${
+          Math.round((metric.startTime + metric.value) * 10) / 10
+        }`
+      );
+      break;
+    default:
+      break;
+  }
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
