7e31ca9d18c9bfce9104cb5f03839829f489527f
otaki0413 <takida0413@gmail.com>
2023-04-25 03:47:15 +0900

    ✨ タスク関連のコンポーネント作成

---
+++ component/task/TaskItem.tsx
@@ -0,0 +1,35 @@
+import { FC } from "react";
+import { useStore } from "@/store";
+import { useMutateTask } from "@/hooks/useMutateTask";
+import { Task } from "@/types/types";
+import { PencilAltIcon, TrashIcon } from "@heroicons/react/solid";
+
+export const TaskItem: FC<Omit<Task, "created_at" | "user_id">> = ({
+  id,
+  title,
+}) => {
+  // タスクの更新関数
+  const update = useStore((state) => state.updateEditedTask);
+  // タスクの削除用ミューテーション
+  const { deleteTaskMutation } = useMutateTask();
+
+  return (
+    <li className="my-3 text-lg font-extrabold">
+      <span>{title}</span>
+      <div className="float-right ml-20 flex">
+        <PencilAltIcon
+          className="mx-1 h-5 w-5 cursor-pointer text-blue-500"
+          onClick={() => {
+            update({ id: id, title: title });
+          }}
+        />
+        <TrashIcon
+          className="h-5 w-5 cursor-pointer text-blue-500"
+          onClick={() => {
+            deleteTaskMutation.mutate(id);
+          }}
+        />
+      </div>
+    </li>
+  );
+};
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,9 @@
 import { NextPage } from "next";
 import { supabase } from "@/lib/supabase";
 import { Layout } from "@/component/Layout";
-import { LogoutIcon } from "@heroicons/react/solid";
+import { DocumentTextIcon, LogoutIcon } from "@heroicons/react/solid";
+import { TaskForm } from "@/component/task/TaskForm";
+import { TaskList } from "@/component/task/TaskList";
 
 const Dashboard: NextPage = () => {
   // サインアウト処理
@@ -10,10 +12,22 @@ const Dashboard: NextPage = () => {
   };
   return (
     <Layout title="Dashboard">
+      {/* ログアウトボタン */}
       <LogoutIcon
         className="mb-6 h-6 w-6 cursor-pointer text-blue-500"
         onClick={signOut}
       />
+      <div className="grid grid-cols-2 gap-40">
+        {/* Task関連 */}
+        <div>
+          <div className="my-3 flex justify-center">
+            <DocumentTextIcon className="h-8 w-8 text-blue-500" />
+          </div>
+          <TaskForm />
+          <TaskList />
+        </div>
+        {/* Notice関連 */}
+      </div>
     </Layout>
   );
 };
